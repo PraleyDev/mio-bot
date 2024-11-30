f332723d1e4dc71e93015dc4bb05ffefef590e88
PraleyDev <gogadaku353@gmail.com>
2024-11-30 17:17:38 +0530

    config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_HASH = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c")
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002094207998"))
 
-CHANNEL_ONE = int(os.environ.get("CHANNEL_ONE", "-1002285339464"))
+CHANNEL_ONE = int(os.environ.get("CHANNEL_ONE", "-1002488967485"))
 CHANNEL_TWO = int(os.environ.get("CHANNEL_TWO", "-1002368514987"))
 
 #OWNER ID
